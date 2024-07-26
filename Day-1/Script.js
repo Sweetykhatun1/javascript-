3ade0f78b4d239f110b46dda577bcc002135fc53
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-07-26 08:01:11 +0600

    delete code

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,32 +1,3 @@
-// variable 
-// variable a 4ti bebohar korte parbo(lettr(a to z), number, dolarsing, andascor(_,-) )
 
-// var Name = "Sweety";
-// document.write(Name);
-
-// Name = "Khatun";
-// document.write(Name);
-
-// var Name = "Sweety Khatun <br>";
-// var age = 19;
-// var Name, age, country;
-
-
-// Name = "Sweety <br>";
-// country = "Bangladesh <br>";
-// age = 19; 
-
-// document.write(Name);
-// document.write(age);
-// document.write(country);
-
-// number method
-// var num = "19";
-// var num = toString(num);
-
-// console.log(typeof(num));
-var num = "19";
-
-console.log(typeof(num));
 
 
