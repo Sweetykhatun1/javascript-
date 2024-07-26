f3832ef5440c59c3992a76f82009e9d80d279909
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-07-26 10:39:17 +0600

    add or concatenate

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,8 +1,14 @@
+var fullName = "Sweety Khatun";
+document.write( "My name is" + fullName + " Ontu" );
+var num1 = 20;
+var num2 = 30;
 
+document.write("Num1 = " +num1 + " And Num2 = " +num2);
 
-var firstName = "Sweety";
-var lastName = " Khatun";
+var carName = "volvo";
+var Top6 = "The Financial Express";
+var Top23 = "Carandbike";
 
-var fullName = firstName + lastName;
-
-document.write( "My Name Js " + fullName);
+document.write("My first choice = " + Top6 + ", second choice = " + carName + 
+    ", third choice = " + Top23
+);
