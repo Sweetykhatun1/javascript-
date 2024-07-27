f5a84a484f0164f6bafd107753515ceb6a66b1cf
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-07-27 18:32:43 +0600

    Relational and Logical Operator

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,14 +1,30 @@
-// fahrenheit to Cecsius
-// Formula = (32°F − 32) × 5/9 = 0°C
-// var fahrenheit = prompt("Enter Fahrenheit : ");
-// var celsius = (fahrenheit - 32) * (5/9);
+// Relational Operator -> >, >=, <, <=, ==, ===, !=, !==
+// Logical Operator -> &&, ||, !
 
-// document.write("Celsius = ", celsius, "°C" );
+var num1 = 20;
+var num2 = 20;
+console.log(num1 >= num2); //num1 er boro ba num1 er soman hole true
 
-// 
-// Formula	= (0°C × 9/5) + 32 = 32°F
-// var celsius = prompt("Enter Fahrenheit : ");
-// var fahrenheit = (celsius * (9/5)) + 32;
+var num1 = 50;
+var num2 = 100;
+console.log(num1 <= num2); //num1 er choto ba num1 er soman hole true
 
-// document.write("Fahrenheit = ", fahrenheit, "°F" );
+console.log(20 == 20);
+console.log(20 == "20"); //(==) mane value check korbe datatype check korbe na
+console.log(20 === "20"); //(===) er kaj value and datatype check kora
+// != value check korbe
+// !== value and datatype check korbe
+console.log(10 != 15); 
 
+
+var num1 = 50;
+var num2 = 30;
+var num3 = 20;
+console.log(num1>num2 && num1>num3); //sb value true hole taholei true return korbe nahole false
+console.log(num1>num2 && num3>num2);
+console.log(num3 > num1 || num3 > num2); //j kono ekta condition true hole true return korbe
+
+console.log(!true);
+console.log(!false);
+console.log(!(25>22));
+console.log(!(25<22));
