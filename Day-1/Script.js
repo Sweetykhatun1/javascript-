5a3aa9bf738be267d90091de1b96650750e8870f
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-07-30 09:41:46 +0600

    switch keyword => switch, case, break, default

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,73 +1,64 @@
-// Control Statement 
-// Conditional Control Statement.
-// Loop Control Statement.
-var num = 6;
-if(num % 2 == 0) {
-    console.log("even");
-}if(num % 2 != 0){
-    console.log("odd");
-};
+// digit spelling
+// switch keyword => switch, case, break, default
 
-var num = 5;
-if(num % 2 == 0) {
-    console.log("even");
-}else {
-    console.log("odd");
+var digit = prompt("Ente any digit : ");
+
+switch(digit){
+    case "0" : {
+        console.log("Zero");
+        break;
+    }case "1": {
+        console.log("One");
+        break;
+    }case "2" : {
+        console.log("Two");
+        break;
+    }case "3": {
+        console.log("Three");
+        break;
+    }case "4" : {
+        console.log("Four");
+        break;
+    }case "5": {
+        console.log("Five");
+        break;
+    }case "6" : {
+        console.log("Six");
+        break;
+    }case "7": {
+        console.log("Seven");
+        break;
+    }case "8" : {
+        console.log("Eight");
+        break;
+    }case "9": {
+        console.log("Nine");
+        break;
+    }default : {
+        console.log("Not a digit");
+    }
 }
 
-var mark = prompt("Enter your marks : ");
-
-if(mark >= 100 || mark <= 0) {
-    console.log("Invalid marks");
-}else if(mark >= 80 && mark <= 100) {
-    console.log("A+");105
-}else if (mark >= 70 && mark <= 79) {
-    console.log("A");
-}else if(mark >= 60 && mark <= 69) {
-    console.log("A-")
-}else if(mark >= 50 && mark <= 59) {
-    console.log("B");
-}else if(mark >= 40 && mark <= 49) {
-    console.log("C");
-}else if(mark >= 33 && mark <= 39) {
-    console.log("D");
-}else{
-    console.log("Fail");
-};
-
-
-
-var num = prompt("Enter a Number : ");
-if(num > 0) {
-    console.log("Positive");
-}
-else if(num < 0) {
-    console.log("Negative");
-}
-else{
+if(digit == 0) {
     console.log("Zero");
-}
-
-
-// Large number create.
-var num1 = prompt("Enter num1 : ");
-var num2 = prompt("Enter num2 : ");
-var num3 = prompt("Enter num2 : ");
-
-if(num1 >= num2 && num1 > num3){
-    console.log(num1,"Large number =",num1 );
-}else if(num2 >= num1 && num2 >= num3){
-    console.log("Large number =",num2 );
+}else if(digit == 1){
+    console.log("One");
+}else if(digit == 2){
+    console.log("Two");
+}else if(digit == 3){
+    console.log("Three");
+}else if(digit == 4){
+    console.log("Four");
+}else if(digit == 5){
+    console.log("Five");
+}else if(digit == 6){
+    console.log("Six");
+}else if(digit == 7){
+    console.log("Seven");
+}else if(digit == 8){
+    console.log("Eight");
+}else if(digit == 9){
+    console.log("Nine");
 }else{
-    console.log("Large number =", num3);
-};
-
-// Vowel / Consonant
-var letter = prompt("Enter a letter : ");
-letter = letter.toLowerCase();
-
-if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"){
-    console.log("Vowel");
-}else {
-    console.log("Consonant");
+    console.log("Not a digit");
 }
