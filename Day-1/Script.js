5886f9e41ebc6bbc17819418136f200ef4f98841
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-07-30 15:50:35 +0600

    logical for loop

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,37 +1,22 @@
-// Task 5 - Input a letter and check it is vowel or consonant using switch
+// document.write("<h1> Bangladesh </h1>");
+// document.write("<h1> Bangladesh </h1>");
+// document.write("<h1> Bangladesh </h1>");
+// document.write("<h1> Bangladesh </h1>");
+// document.write("<h1> Bangladesh </h1>");
 
-var letter = prompt("Enter any letter : ");
-switch(letter){
-    case "a" : {
-        console.log("vowel");
-        break;
-    }case "e" : {
-        console.log("vowel");
-        break;
-    }case "i" : {
-        console.log("vowel");
-        break;
-    }case "o" : {
-        console.log("vowel");
-        break;
-    }case "u" : {
-        console.log("vowel");
-        break;
-    }default : {
-        console.log("letter a consonant");
-    }
-};
+var user = parseFloat(prompt("Enter your Number : "));
+var num = parseFloat(prompt("Enter your condition Number : "));
+var sum = 0;
+for(var x = user;  x <= num;  x = x+1){
+     sum = sum + x;
+}
+console.log((sum));
 
-var letter = prompt("Enter any letter : ");
-switch(letter){
-    case "a" : 
-    case "e" : 
-    case "i" : 
-    case "o" : 
-    case "u" : 
-        console.log("vowel");
-        break;
-    default : {
-        console.log("consonant");
-    }
-};
+for(var x = 1; x <= 2; x++){
+    var num1 = parseFloat(prompt("Enter first number : "));
+    var num2 = parseFloat(prompt("Enter last number : "));
+    var sum = num1 + num2;
+console.log(sum);
+}
+
+
