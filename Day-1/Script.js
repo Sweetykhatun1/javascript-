c82b3b9addc45c75073ba5117731c9f2f3a8d124
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-07-30 09:54:27 +0600

    Task 5 - Input a letter and check it is vowel or consonant using switch.

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,64 +1,37 @@
-// digit spelling
-// switch keyword => switch, case, break, default
+// Task 5 - Input a letter and check it is vowel or consonant using switch
 
-var digit = prompt("Ente any digit : ");
-
-switch(digit){
-    case "0" : {
-        console.log("Zero");
+var letter = prompt("Enter any letter : ");
+switch(letter){
+    case "a" : {
+        console.log("vowel");
         break;
-    }case "1": {
-        console.log("One");
+    }case "e" : {
+        console.log("vowel");
         break;
-    }case "2" : {
-        console.log("Two");
+    }case "i" : {
+        console.log("vowel");
         break;
-    }case "3": {
-        console.log("Three");
+    }case "o" : {
+        console.log("vowel");
         break;
-    }case "4" : {
-        console.log("Four");
-        break;
-    }case "5": {
-        console.log("Five");
-        break;
-    }case "6" : {
-        console.log("Six");
-        break;
-    }case "7": {
-        console.log("Seven");
-        break;
-    }case "8" : {
-        console.log("Eight");
-        break;
-    }case "9": {
-        console.log("Nine");
+    }case "u" : {
+        console.log("vowel");
         break;
     }default : {
-        console.log("Not a digit");
+        console.log("letter a consonant");
     }
-}
+};
 
-if(digit == 0) {
-    console.log("Zero");
-}else if(digit == 1){
-    console.log("One");
-}else if(digit == 2){
-    console.log("Two");
-}else if(digit == 3){
-    console.log("Three");
-}else if(digit == 4){
-    console.log("Four");
-}else if(digit == 5){
-    console.log("Five");
-}else if(digit == 6){
-    console.log("Six");
-}else if(digit == 7){
-    console.log("Seven");
-}else if(digit == 8){
-    console.log("Eight");
-}else if(digit == 9){
-    console.log("Nine");
-}else{
-    console.log("Not a digit");
-}
+var letter = prompt("Enter any letter : ");
+switch(letter){
+    case "a" : 
+    case "e" : 
+    case "i" : 
+    case "o" : 
+    case "u" : 
+        console.log("vowel");
+        break;
+    default : {
+        console.log("consonant");
+    }
+};
