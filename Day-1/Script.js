a13e90913e75a43db338a6eec609dac612084f49
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-07-31 23:02:07 +0600

    while-loop

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,16 +1,27 @@
 for(var i = 0; i <= 100; i++){
     console.log(i);
 }
+//task -> 6 write a program that will print sum of all number that are divisible by 3 and 5 from 1-100.
 
 var i = 1;
-var sum = 0;
 while(i <= 50){
-    if(i%3 == 0 && i%5 == 0){
-        console.log(" ", i);
+    if(i%3 == 0 && i%5 == 0) {
+        console.log("", i);
         sum = sum + i;
     }
     i++;
 }
 console.log(sum);
+
+var i = 0;
+while(i <= 100) {
+    console.log(i);
+    i++;
+}
+
+for(var i = 1; i <= 100; i++){
+    console.log(i);
+};
+
 
 
