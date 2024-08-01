b9a101b93bc047b0f3496017a07ce51a662f671b
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-08-01 14:01:22 +0600

    IIFEs (Immediately Invokeable Functin Expressions).

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,45 +1,27 @@
-var num = 5;
-var result = num * num;
-console.log(result, "<br>");
+// IIFEs (Immediately Invokeable Functin Expressions)
 
-var num = 6;
-var result = num * num;
-console.log(result, "<br>");
-
-var num = 7;
-var result = num * num;
-console.log(result, "<br>");
-
-// creating a function
-function sqaure(num1, num2) {
-    var result = num1 * num2;
-    return result;
+function display () {
+    console.log('hello');
 }
-console.log(sqaure(5,6));
+display();
 
-function sum(num3, num4, num5){
-    let res = num3 + num4 - num5;
-    return res;
-};
-console.log(sum(30, 20, 5));
+(function display (message) {
+    console.log(message);
+})("Hello");
 
-// add function
-function addition(x, y){
-    let res = x + y;
-    console.log(res, "<br>");
+// Task 7
+(function sum(a, b){
+    console.log(a + b);
+})(5, 9);
+
+// Function Expression
+const display2 = function displayMessage(msg) {
+    console.log(msg);
 }
-addition(30, 20);
-// sub function
-function subtraction(z, x){
-    let res = z - x;
-    console.log(res, "<br>");
-}
+display2('I am message');
 
-subtraction(30, 20);
-subtraction(60, 30);
-subtraction(80, 5);
+// () => "parantasis",
+// {} => "karli braket",
+// [] => "square braket",
+// <> => "andor braket";
 
-// create a function
-// calling a function
-// how to pass value in function
-// returning a value
