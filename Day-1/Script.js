2680c5d0e6e1fc3ba0a1f9c8aa0bcb27a56cc824
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-08-01 13:39:40 +0600

    Traditional function

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,23 +1,45 @@
-// Ternary operator
-var num = Number(prompt("Enter a number : "));
-if(num > 0) {
-    console.log("Positive");
-}else{
-    console.log("negative");
+var num = 5;
+var result = num * num;
+console.log(result, "<br>");
+
+var num = 6;
+var result = num * num;
+console.log(result, "<br>");
+
+var num = 7;
+var result = num * num;
+console.log(result, "<br>");
+
+// creating a function
+function sqaure(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
+console.log(sqaure(5,6));
+
+function sum(num3, num4, num5){
+    let res = num3 + num4 - num5;
+    return res;
 };
+console.log(sum(30, 20, 5));
 
-var result = num > 0? "positive" : "negative";
-console.log(result);
+// add function
+function addition(x, y){
+    let res = x + y;
+    console.log(res, "<br>");
+}
+addition(30, 20);
+// sub function
+function subtraction(z, x){
+    let res = z - x;
+    console.log(res, "<br>");
+}
 
-// ternary operator a 2 er othik condition thakle ki hob?
-if(num > 0) {
-    console.log("Positive");
-}else if(num < 0 ){
-    console.log("negative");
-}else{
-    console.log("Zero");
-};
+subtraction(30, 20);
+subtraction(60, 30);
+subtraction(80, 5);
 
-var num = Number(prompt("Enter a number : "));
-var result = num > 0 ? "positive" : num < 0 ? "negative" : "Zero";
-console.log(result);
+// create a function
+// calling a function
+// how to pass value in function
+// returning a value
