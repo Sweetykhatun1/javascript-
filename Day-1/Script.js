91b291922284ce631e19c44bdc9b46bb408ed1ea
SweetyKhatun <sweetykhatun0011@gmail.com>
2024-08-01 09:45:50 +0600

    Ternary Operator

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,22 +1,23 @@
-// break and continue explain
-
-for(i=1; i <= 100; i++) {
-    if(i == 10){
-        break;
-    }
-    console.log(i);
+// Ternary operator
+var num = Number(prompt("Enter a number : "));
+if(num > 0) {
+    console.log("Positive");
+}else{
+    console.log("negative");
 };
 
-for(i=1; i <= 100; i++) {
-    console.log(i);
-    if(i == 10){
-        break;
-    }
+var result = num > 0? "positive" : "negative";
+console.log(result);
+
+// ternary operator a 2 er othik condition thakle ki hob?
+if(num > 0) {
+    console.log("Positive");
+}else if(num < 0 ){
+    console.log("negative");
+}else{
+    console.log("Zero");
 };
 
-for(i=1; i <= 100; i++) {
-    if(i%2 != 0){
-        continue;
-    }
-    console.log(i);
-};
+var num = Number(prompt("Enter a number : "));
+var result = num > 0 ? "positive" : num < 0 ? "negative" : "Zero";
+console.log(result);
