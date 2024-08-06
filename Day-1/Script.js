6b7316a2a6cae499c8f19517315e531ce4f586db
SweetyKhatun <sweetyakter6053@gmail.com>
2024-08-06 11:25:47 +0600

    2d Array task 9

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,22 +1,66 @@
-// One dimensional (1D) Array
-let motorCycle = ["Tarform Motorcycles", "Janus Motorcycle", 
-    "Cleveland CycleWerks", "Damon Motorcycles", "Harley-Davidson"];
+// Two dimensional (2D) Array Creating
 
-    for(Top = 0; Top < 5; Top++) {
-        console.log(motorCycle[Top]);
-    }
-
-
-function highestScore(scores){ 
-        let high = scores[0];
-        for(x = 1; x < scores.length; x++) {
-            if(high < scores[x]) {
-                high = scores[x];
-            }
+function highestScore (scores){
+    let max = scores[0];
+    for(let x = 1; x < scores.length; x++) {
+        if(max < scores[x]) {
+            max = scores[x];
         }
-        return high;
+    } 
+    return max;                                                                       
+}
+let scores = [23, 20, 25, 67, 100, 34, 75,43];
+let maxScore = highestScore(scores);
+console.log(maxScore);
+
+
+function info(Scores) {
+    let highersScore = Scores[0][0];
+    let higherScore = Scores[0][1];
+
+    for(let x = 1; x < Scores.length; x++){
+        if(higherScore < Scores[x][1]) {
+            higherScore = Scores[x][1];
+            highersScore = Scores[x][0];
+        }
     }
-    let scores = [34, 54, 200, 75, 43, 12, 90, 100];
-    let highScore = highestScore(scores); 
-    console.log(highScore);      
-    
+    return highersScore;
+}
+
+let Scores = [ 
+    ["Ashraful", 95],
+    ["Sakib", 19],
+    ["Mushfiq", 103],
+    ["Mahmudullah", 45],
+    ["Mashrafee", 98]
+   ];
+
+   let name = info(Scores);
+   console.log(name);
+
+
+function totalResult (playersInfo){
+    let highestScorerName = playersInfo[0][0];
+    let highestScore = playersInfo[0][1];
+
+    for(let x = 1; x < playersInfo.length; x++){
+        if(highestScore < playersInfo[x][1]){
+            highestScore = playersInfo[x][1];
+            highestScorerName = playersInfo[x][0];
+        }
+    }
+    return highestScorerName ;
+};
+
+let playersInfo = [
+    ["Lionel Messi", 49], 
+    ["Cristiano Ronaldo", 180], 
+    ["Neymar", 89], 
+    ["Robert Lewandowski", 59],
+    ["KylianMbappe", 69], 
+    ["Kevin De Bruyne", 70], 
+    ["Virgil van Dijk", 87],
+];
+
+let playerHighestScore = totalResult(playersInfo);
+console.log(playerHighestScore);
