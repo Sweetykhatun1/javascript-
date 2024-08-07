dd3401f15bf0a70bf65d7d0bca6a108db14231a6
SweetyKhatun <sweetyakter6053@gmail.com>
2024-08-07 12:01:34 +0600

    Id, class, tag select, new element create

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,59 +1,51 @@
-// let val = Math.sqrt(25);
-// console.log(val);
-// let valu = Math.abs(-5);
-// console.log(valu);
-// let value = Math.sin(2);
-// console.log(value);
-// let value1 = Math.pow(2,4);
-// console.log(value1);
-// let value2 = Math.floor(9);
-// console.log(value2);
-// let value3 = Math.ceil(2.1);
-// console.log(value3);
-// let value4 = Math.round(3.5);
-// console.log(value4);
-// let value5 = Math.max(3,9,7);
-// console.log(value5);
-// let value6 = Math.random() * 6;
-// console.log(value6);
-// let value7 = Math.floor(Math.random() * 6);
-// console.log(value7);
-// let value8 = Math.floor(Math.random() * 6);
-// console.log(value8);
-// let value9 = Math.floor(Math.random() * 6);
-// console.log(value9);
-// let value10 = Math.floor(Math.random() * 6);
-// console.log(value10);
-// let value11 = Math.floor(Math.random() * 11) + 10;
-// console.log(value11);
-// let value12 = Math.floor(Math.random() * 11) + 10;
-// console.log(value12);
-// let value13 = Math.floor(Math.random() * 11) + 10;
-// console.log(value13);
-// let value14 = Math.floor(Math.random() * 11) + 10;
-// console.log(value14);
-// let value15 = Math.floor(Math.random() * 11) + 10;
-// console.log(value15);
-// let value16 = Math.floor(Math.random() * 11) + 10;
-// console.log(value16);
-// let value17 = Math.floor(Math.random() * 11) + 10;
-// console.log(value17);
+// Date object
+let date = new Date();
+console.log(date);
+let year = date.getFullYear();
+console.log(year);
+let month = date.getMonth();
+console.log(month);
+let curr = date.getDay();
+console.log(curr);
+let currHour = date.getHours();
+console.log(currHour);
+let currS = date.getSeconds();
+console.log(currS);
+let botton = document.getElementById("click");
+botton.addEventListener(click);
 
-let Win = 0;
-let Lost = 0;
 
-for(let i = 1; i <= 5; i++){
-    let guessNumber = parseInt(prompt("Enter a number from 1  to 5 : "));
-console.log(guessNumber);
+let h1 = document.getElementById("heading1");
+h1.innerHTML = "Hello";
+console.log(h1);
 
-let ranNumber = Math.floor(Math.random( ) * 5) + 1;
-if(guessNumber === ranNumber){
-    console.log("you have won");
-    Win++;
-}else{
-    console.log("you have lost. Random number was ", ranNumber);
-    Lost++;
-}
-};
-console.log("Number of won =",Win);
-console.log("Number of lost =",Lost);
+let p = document.getElementById("para");
+p.innerHTML = "Bye";
+console.log(p);
+
+let H1 = document.getElementById("heading2");
+H1.innerHTML = "Good Bye";
+console.log(H1);
+
+let tag = document.getElementsByTagName("h1")[1];
+tag.innerHTML = "My name";
+console.log(tag);
+
+let para = document.getElementsByClassName("para1")[0];
+para.innerHTML = "MYself";
+console.log(para);
+
+querySelector
+let pa = document.querySelector(".pag");
+pa.innerHTML = "Institute";
+console.log(pa);
+let pa = document.querySelector("p");
+pa.innerHTML = "Institute";
+console.log(pa);
+
+let a = document.querySelector("li a").innerHTML = "House";
+
+let div = document.querySelector(".my-div a").innerHTML = "Communication";
+
+let all = document.querySelectorAll("p")[0].innerHTML = "New Paragraph";
+console.log(all);
