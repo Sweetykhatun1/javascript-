a4e8fab9374b30a98f9bf89394fdb036cb5c2384
SweetyKhatun <sweetyakter6053@gmail.com>
2024-08-07 10:45:33 +0600

    Math methods and project

--- Day-1/Script.js
+++ Day-1/Script.js
@@ -1,66 +1,59 @@
-// Two dimensional (2D) Array Creating
+// let val = Math.sqrt(25);
+// console.log(val);
+// let valu = Math.abs(-5);
+// console.log(valu);
+// let value = Math.sin(2);
+// console.log(value);
+// let value1 = Math.pow(2,4);
+// console.log(value1);
+// let value2 = Math.floor(9);
+// console.log(value2);
+// let value3 = Math.ceil(2.1);
+// console.log(value3);
+// let value4 = Math.round(3.5);
+// console.log(value4);
+// let value5 = Math.max(3,9,7);
+// console.log(value5);
+// let value6 = Math.random() * 6;
+// console.log(value6);
+// let value7 = Math.floor(Math.random() * 6);
+// console.log(value7);
+// let value8 = Math.floor(Math.random() * 6);
+// console.log(value8);
+// let value9 = Math.floor(Math.random() * 6);
+// console.log(value9);
+// let value10 = Math.floor(Math.random() * 6);
+// console.log(value10);
+// let value11 = Math.floor(Math.random() * 11) + 10;
+// console.log(value11);
+// let value12 = Math.floor(Math.random() * 11) + 10;
+// console.log(value12);
+// let value13 = Math.floor(Math.random() * 11) + 10;
+// console.log(value13);
+// let value14 = Math.floor(Math.random() * 11) + 10;
+// console.log(value14);
+// let value15 = Math.floor(Math.random() * 11) + 10;
+// console.log(value15);
+// let value16 = Math.floor(Math.random() * 11) + 10;
+// console.log(value16);
+// let value17 = Math.floor(Math.random() * 11) + 10;
+// console.log(value17);
 
-function highestScore (scores){
-    let max = scores[0];
-    for(let x = 1; x < scores.length; x++) {
-        if(max < scores[x]) {
-            max = scores[x];
-        }
-    } 
-    return max;                                                                       
+let Win = 0;
+let Lost = 0;
+
+for(let i = 1; i <= 5; i++){
+    let guessNumber = parseInt(prompt("Enter a number from 1  to 5 : "));
+console.log(guessNumber);
+
+let ranNumber = Math.floor(Math.random( ) * 5) + 1;
+if(guessNumber === ranNumber){
+    console.log("you have won");
+    Win++;
+}else{
+    console.log("you have lost. Random number was ", ranNumber);
+    Lost++;
 }
-let scores = [23, 20, 25, 67, 100, 34, 75,43];
-let maxScore = highestScore(scores);
-console.log(maxScore);
-
-
-function info(Scores) {
-    let highersScore = Scores[0][0];
-    let higherScore = Scores[0][1];
-
-    for(let x = 1; x < Scores.length; x++){
-        if(higherScore < Scores[x][1]) {
-            higherScore = Scores[x][1];
-            highersScore = Scores[x][0];
-        }
-    }
-    return highersScore;
-}
-
-let Scores = [ 
-    ["Ashraful", 95],
-    ["Sakib", 19],
-    ["Mushfiq", 103],
-    ["Mahmudullah", 45],
-    ["Mashrafee", 98]
-   ];
-
-   let name = info(Scores);
-   console.log(name);
-
-
-function totalResult (playersInfo){
-    let highestScorerName = playersInfo[0][0];
-    let highestScore = playersInfo[0][1];
-
-    for(let x = 1; x < playersInfo.length; x++){
-        if(highestScore < playersInfo[x][1]){
-            highestScore = playersInfo[x][1];
-            highestScorerName = playersInfo[x][0];
-        }
-    }
-    return highestScorerName ;
 };
-
-let playersInfo = [
-    ["Lionel Messi", 49], 
-    ["Cristiano Ronaldo", 180], 
-    ["Neymar", 89], 
-    ["Robert Lewandowski", 59],
-    ["KylianMbappe", 69], 
-    ["Kevin De Bruyne", 70], 
-    ["Virgil van Dijk", 87],
-];
-
-let playerHighestScore = totalResult(playersInfo);
-console.log(playerHighestScore);
+console.log("Number of won =",Win);
+console.log("Number of lost =",Lost);
